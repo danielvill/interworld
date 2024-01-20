e96cb95c1597bef7852b7492b25cc8c5a7e1b582
Daniel Villacres <93736800+danielvill@users.noreply.github.com>
2024-01-20 14:33:48 -0500

    Add files via upload

---
+++ contexto.py
@@ -0,0 +1,31 @@
+# Lo que tendra esta aplicacion es lo siguiente 
+
+#Lado de Registro y recuperacion de contraseña y forma como lo hara
+login = "registrarse"
+olcontraseña = "recuperacion de contraseña"
+recuperacion = "este lado pondra el correo para enviar recuperacion contraseña"
+recuusuario = "Cuando el usuario tenga el codigo se le dira que vaya al apartado actualizacion de contraseña"
+registrarse=["usuario","correo","contraseña"] 
+
+
+#Adentro del sistema de lado del cliente
+
+#Modulo clientes
+agendar =["hora","telefono","fecha","direccion","provincia"]#Detalle solo los administradores podra visualizar los telefonos para la visita tecnica
+vistaage=["hora","fecha","provincia"] #Podra visualizar maximo 10 registro de otros clientes con los datos que se agrego
+usuarios=["usuario","contraseña","corre"]# EL cual se tendra funciones para actulizar su usuario
+
+
+#Adentro del sistema de Tecnicos
+#Modulo Tecnicos
+
+clientes="Vista de clientes para editar o eliminar a los usuarios que no desean estar hay"
+vistagenda="vista de todos los clientes pero por fecha actual no saldra todos los registros sino actuales o fecha de hoy mismo"
+# la vistagenda tendra un boton que diga completado y este eliminara de vista cliente la cita y ya no la veran mas
+# Para poder visualizar por fecha actual y que tenda el filtro de visualizar por fechas que uno elija para atras
+
+vistacompleta="esta vista tendra que hacer para visualizar todas las visitas completadas "
+
+agregadotecnicao="esta vista es para agregar tecnicos y mas abajo editarlos o eliminarlos"
+
+vistarecuperacion="donde se vera las recuperaciones que piden los usuarios para cambio de clave"
